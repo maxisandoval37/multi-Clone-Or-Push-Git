04c58625539d400f78ba6e78adb5461768f29de0
Maximiliano Sandoval <maxisandoval98@gmail.com>
2022-07-28 13:51:51 -0500

    Update SCRIPT PUSH INIT GIT.bat

--- SCRIPT PUSH INIT GIT.bat
+++ SCRIPT PUSH INIT GIT.bat
@@ -36,7 +36,7 @@ for /f "tokens=*" %%h in (repo_list.txt) do (
 	git init
 	git remote add origin https://mySite@dev.azure.com/repositories/_git/!repoName!
 	git add .
-	git commit -m "firt commit"
+	git commit -m "first commit"
 	git push -u origin --all
 	cd ..
 
